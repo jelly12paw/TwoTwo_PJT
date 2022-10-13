6666aa56133a479afb9b7fe86d4ef688c872a443
jincde <asdzxc@kakao.com>
2022-10-13 17:16:07 +0900

    Add: 게시판 메인, 디테일

--- twotwo/articles/views.py
+++ twotwo/articles/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Article
 
 # Create your views here.
+def index(request):
+    articles = Article.objects.order_by('-pk')
+
+    context = {
+        'articles': articles
+    }
+    return render(request, 'articles/index.html', context)
+
+def detail(request, pk):
+
+    article = Article.objects.get(id=pk)
+
+    context = {
+        'article': article
+    }
+    
+    return render(request, 'articles/detail.html', context)
