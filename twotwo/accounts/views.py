0fbe9897f0fb1772b34bd025f522ae9f0821d2f9
avocad06 <miggo2704@gmail.com>
2022-10-13 16:49:02 +0900

    Create : login_function

--- twotwo/accounts/views.py
+++ twotwo/accounts/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 
 # Create your views here.
+def login(request):
+    if request.method == 'POST':
+        # AuthenticationForm은 ModelForm이 아님!
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            # 세션에 저장
+            # login 함수는 request, user 객체를 인자로 받음 
+            # user 객체는 어디있어요? 바로 form에서 인증된 유저 정보를 받을 수 있음
+            auth_login(request, form.get_user())
+            # http://127.0.0.1:8000/accounts/login/?next=/articles/1/update/
+            # request.GET.get('next') : /articles/1/update/
+            return redirect(request.GET.get('next') or 'articles:index')
+    else:
+        form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/login.html', context)
+
+def logout(request):
+    auth_logout(request)
+    return redirect('articles:index')
