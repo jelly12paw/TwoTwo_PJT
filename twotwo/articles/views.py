859747f87f61df8b8e825259abbc9e51d8c40ded
kimdakyeom <74820869+kimdakyeom@users.noreply.github.com>
2022-10-13 16:55:37 +0900

    board_update

--- twotwo/articles/views.py
+++ twotwo/articles/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from articles.forms import ArticleForm
+from .models import Article
 
-# Create your views here.
+def update(request, pk):
+    articles = Article.objects.get(pk=pk)
+    if request.method == "POST":
+        article_form = ArticleForm(request.POST, instance=articles)
+        if article_form.is_valid():
+            article_form.save()
+            return redirect("accounts:index")
+    else:
+        article_form = ArticleForm(instance=articles)
+    context = {
+        "article_form": article_form,
+        "articles": articles,
+    }
+    return render(request, 'articles/update.html', context)
