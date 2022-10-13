b4b6e326c65ba74d9f3dc2f1d3737120d91e9849
wnsn8546 <wnsn8546@gmail.com>
2022-10-13 16:50:03 +0900

    add ArticlesCreateDelete

--- twotwo/accounts/urls.py
+++ twotwo/accounts/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'accounts'
 
 urlpatterns = [
+    path('create/', views.create, name='create'),
     path('password/', views.change_password, name='change_password'),
+    path('delete/<int:pk>', views.delete, name="delete"),
 ]
