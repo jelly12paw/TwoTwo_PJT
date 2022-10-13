4590802c2d3aeca69b49f7e30e360810d42f677f
HYE <jelly12paw@gmail.com>
2022-10-13 15:55:42 +0900

    make twotwo_pjt

---
+++ twotwo/accounts/forms.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+# from .models import User
+from django.contrib.auth import get_user_model
+
+class UsersForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = ('username', 'first_name', 'last_name', 'email')
+
+class CustomUserChangeForm(UserChangeForm):
+
+    class Meta:
+        model = get_user_model()
+        fields = ('first_name', 'last_name', 'email')
